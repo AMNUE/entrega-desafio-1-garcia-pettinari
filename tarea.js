34cffa036389af0b0f95f408d7c5b1188161cc48
Manuel Garcia Pettinari <manuel.garciapettinari@gmail.com>
2023-05-06 03:02:00 -0300

    Correcciones

--- tarea.js
+++ tarea.js
@@ -8,16 +8,42 @@ class ProductManager {
         return this.products;
     }
 
-    buscarProductos(id) {
+    getProductsById(id) {
         const found = this.products.find((prod) => prod.id == id);
-        if (found) {
+        if (found){
             return found;
-        }else {
-            return undefined;
+        } else {
+            console.log("Not Found");
         }
+        
+        
     }
-    addProduct(title, description, price, thumbnail, code, stock){
-        nombre
+    #generateId () {
+        let maxId = 0;
+        for (let i = 0; i < this.products.length; i++){
+            const prod = this.products[i];
+            if (prod.id > maxId) {
+                maxId = prod.id;
+            }
+        }
+        return ++maxId;
 
     }
+    addProduct(title, description, price, thumbnail, code, stock){
+        
+        let newProduct = { title, description, price, thumbnail, code, stock, id: this.#generateId() };
+        this.products = [...this.products, newProduct];
+        return true;
+        
+
+
+    }
+    
 }
+
+const generator = new ProductManager();
+
+generator.addProduct("Producto prueba", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25);
+
+const found = generator.getProductsById(1)
+console.log(generator.products);
